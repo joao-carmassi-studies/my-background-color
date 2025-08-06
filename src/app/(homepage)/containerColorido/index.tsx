626dc2d07f486a63cec26f8c7ef8e87ac7eb9762
Joao Carmassi <joaovitorcarmassi@gmail.com>
2025-08-06 00:41:36 -0300

    scroll bar muda cor

--- src/app/(homepage)/containerColorido/index.tsx
+++ src/app/(homepage)/containerColorido/index.tsx
@@ -6,7 +6,38 @@ const ContainerColorido = () => {
   const { color } = useAppContext();
 
   return (
-    <section style={{ backgroundColor: color }} className='min-h-screen' />
+    <>
+      <section style={{ backgroundColor: color }} className='min-h-screen' />
+      <style>
+        {`
+          body {
+            --sb-track-color: ${color};
+            --sb-thumb-color: ${color};
+            --sb-size: 8px;
+          }
+
+          body::-webkit-scrollbar {
+            width: var(--sb-size);
+          }
+
+          body::-webkit-scrollbar-track {
+            background: var(--sb-track-color);
+            border-radius: 12px;
+          }
+
+          body::-webkit-scrollbar-thumb {
+            background: var(--sb-thumb-color);
+            border-radius: 0px;
+          }
+
+          @supports not selector(::-webkit-scrollbar) {
+            body {
+              scrollbar-color: var(--sb-thumb-color) var(--sb-track-color);
+            }
+          }
+        `}
+      </style>
+    </>
   );
 };
 
